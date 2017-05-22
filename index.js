69b4fb8866b4d9627042252f1246d5de509c87a3
Joxxorz <jockperkins@jocks-mbp.lsm.local>
2017-05-22 14:49:37 +0100

    * still trying to make multi bar work

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ exports.triggerBar = function(barId){
     console.log('not this time');
   }
   else {
-    document.getElementById(barId).find('div').style.width = "100%";
+    document.getElementById(barId).children('div').style.width = "100%";
   }
   //var theBar = '#' + barId + ' .loading-bar';
   //document.getElementById(barId).getElementsByClassName("loading-bar").style.width = "100%";
