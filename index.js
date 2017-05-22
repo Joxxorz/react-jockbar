3bb244b1d9e51598ac82ec1881ca89da47e55a07
Joxxorz <jockperkins@jocks-mbp.lsm.local>
2017-05-22 14:42:29 +0100

    * updated how id gets found to attempt to make it work

--- index.js
+++ index.js
@@ -8,5 +8,8 @@ exports.progressBar = function(width, height, color, time) {
 }
 
 exports.triggerBar = function(barId){
-  document.getElementById(barId).getElementsByClassName("loading-bar").style.width = "100%";
+  //document.getElementById(barId);
+  //var theBar = '#' + barId + ' .loading-bar';
+  //document.getElementById(barId).getElementsByClassName("loading-bar").style.width = "100%";
+  document.getElementById(barId).find('div').style.width = "100%";
 }
