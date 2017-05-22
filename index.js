73bc3dd0228b04f041c13a2dfb5a0e15bc04e57d
Joxxorz <jockperkins@jocks-mbp.lsm.local>
2017-05-22 14:45:55 +0100

    * further updates to the function for multi bar

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ exports.progressBar = function(width, height, color, time) {
 
 exports.triggerBar = function(barId){
   //document.getElementById(barId);
+  barId = barId || 'none';
+  if(barId === 'none'){
+    console.log('not this time');
+  }
+  else {
+    document.getElementById(barId).find('div').style.width = "100%";
+  }
   //var theBar = '#' + barId + ' .loading-bar';
   //document.getElementById(barId).getElementsByClassName("loading-bar").style.width = "100%";
-  document.getElementById(barId).find('div').style.width = "100%";
+
 }
